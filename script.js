5685392cc607518ed634070a5ff371724d1c1b66
MrChub467 <173291253+MrChub467@users.noreply.github.com>
2024-08-27 18:44:14 -0500

    Add stuff

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
-const boxesContainer = document.querySelector("#container");
-
-const gridSizeSlider = document.querySelector("#grid-size-slider");
-let gridSizeLabel = document.querySelector("#grid-size"); 
-let gridArray = [];
-
-
-
-function emptyGrid() {
+function emptyGrid(parent) {
   gridArray.forEach(child => {
-    boxesContainer.removeChild(child);
+    parent.removeChild(child);
   });
   gridArray.length = 0;
 }
 
 function changeGridSize() {
-  emptyGrid();
+  
+  emptyGrid(boxesContainer);
   for (let i = 0; i < (gridSizeSlider.value)**2; i++) {
     let newBox = document.createElement("div");
     newBox.classList.add("box");
@@ -26,21 +19,46 @@ function changeGridSize() {
     box.style.flexBasis = newFlexBasis + "%";
   });
 }
-
+const boxesContainer = document.querySelector("#boxes-container");
+let isMouseDown = false;
+boxesContainer.addEventListener("mousedown", (e) => {
+  isMouseDown = true;
+  e.target.style.backgroundColor = colorPicker.value;
+  e.preventDefault();
+});
+document.addEventListener("mouseup", () => {
+  isMouseDown = false;
+});
 document.addEventListener("mouseover", (e) => {
-  if (e.target.className === "box") {
-    if (e.target.style.backgroundColor === "black") {
-      e.target.style.backgroundColor = "";
-    }
-    else {
-      e.target.style.backgroundColor = "black";
-    }
+  if (e.target.className === "box" && isMouseDown) {
+    e.target.style.backgroundColor = colorPicker.value;
   }
 });
 
-document.addEventListener("input", (e) => {
+const gridSizeSlider = document.querySelector("#grid-size-slider");
+gridSizeSlider.value = "16";
+let gridSizeLabel = document.querySelector("#grid-size"); 
+gridSizeLabel.textContent = "Grid Size: " + gridSizeSlider.value;
+let colorPicker = document.querySelector("#color-picker");
+let gridArray = [];
+
+changeGridSize();
+
+// Grid Size Slider event listeners to disable the arrow keys, change the size of the grip when the mouse is released, and to change the text of the label
+gridSizeSlider.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowUp" || e.key === "ArrowDown" ||
+      e.key === "ArrowLeft" || e.key === "ArrowRight") {
+        e.preventDefault();
+      }
+});
+gridSizeSlider.addEventListener("input", (e) => {
   gridSizeLabel.textContent = "Grid Size: " + e.target.value;
+});
+gridSizeSlider.addEventListener("mouseup", (e) => {
   changeGridSize();
 });
 
-changeGridSize();
+// 
+
+
+
